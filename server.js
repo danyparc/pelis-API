41c4aefefb53e7ee67361b50a8ba706047031bb6
danyparc <danyparc@gmail.com>
2019-04-10 20:22:54 -0500

    port updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Pelicula } = require('./models/Pelicula');
 const cors = require('cors');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
